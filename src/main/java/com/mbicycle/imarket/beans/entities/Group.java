574f8103cebd72913ba2cfa798b47daed3c29ba9
Админ <decurt_gogencolern@mail.ru>
2018-12-19 14:21:26 +0300

    Group and Category entities realized;

--- src/main/java/com/mbicycle/imarket/beans/entities/Group.java
+++ src/main/java/com/mbicycle/imarket/beans/entities/Group.java
@@ -1,15 +1,68 @@
 package com.mbicycle.imarket.beans.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Table(name = "groups")
 public class Group {
+
+    @Id
+    @SequenceGenerator(name = "groups_sequence_generator", sequenceName = "groups_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "groups_sequence_generator")
     private int id;
+
     private String name;
 
+    @ManyToOne
+    @JoinColumn(name = "id_category", nullable = false)
     private Category category;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Product> products;
+
+    public Group() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category=" + category +
+                ", products=" + products +
+                '}';
+    }
 }
