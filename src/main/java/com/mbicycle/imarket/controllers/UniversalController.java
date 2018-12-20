da7eea3c90ceaed24dc8b4778f8a0f6b3829477c
Админ <decurt_gogencolern@mail.ru>
2018-12-20 17:42:24 +0300

    UniversalController mappings changed;

--- src/main/java/com/mbicycle/imarket/controllers/UniversalController.java
+++ src/main/java/com/mbicycle/imarket/controllers/UniversalController.java
@@ -1,73 +1,105 @@
 package com.mbicycle.imarket.controllers;
 
-import com.mbicycle.imarket.beans.entities.Product;
-import com.mbicycle.imarket.daos.ProductRepository;
+import com.mbicycle.imarket.beans.entities.*;
+import com.mbicycle.imarket.daos.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class UniversalController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/index")
-    public String getIndex() {
-        return "-0-";
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users/allUsersSortedByLogin")
+    public List<User> getAllUsersSortedByLogin() {
+        return userRepository.getAllSortedByLogin();
     }
 
-    @GetMapping("/search")
-    public String getSearch() {
-        return "-";
-    }    
-    
-
-    @GetMapping(value = "/products", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    @GetMapping("profiles/allProfilesSortedByName")
+    public List<Profile> getAllProfilesSortedByName() {
+        return profileRepository.getAllSortedByName();
     }
 
-    @GetMapping(value = "/product", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public Optional<Product> getProduct(@RequestParam("id") int id) {
-        return productRepository.findById(id);
+    @GetMapping("/roles/allRolesSortedByRole")
+    public List<Role> getAllRolesSortedByRole() {
+        return roleRepository.getAllSortedByRole();
     }
 
-
-    @PostMapping(value = "/products/add", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public @ResponseBody Product getProductsAdd(@RequestBody Product product) {
-        return productRepository.save(product);//тут надо редирект Product page?
+    @GetMapping("/categories/allCategoriesSortedByName")
+    public List<Category> getAllCategoriesSortedByName() {
+        return categoryRepository.getAllSortedByName();
     }
 
-
-
-    @GetMapping("/products/add")
-    public String getProductsAdd() {
-        return "-0-";
-    }    
-    
-    @GetMapping("/products/edit")
-    public String getProductsEdit() {
-        return "-0-";
-    }    
-    
-    @GetMapping("/products/delete")
-    public String getProductsDelete() {
-        return "-0-";
-    }    
-    
-    @GetMapping("/cart")
-    public String getCart() {
-        return "-0-";
+    @GetMapping("/groups/allGroupsSortedByName")
+    public List<Group> getAllGroupsSortedByName() {
+        return groupRepository.getAllSortedByName();
     }
 
-    @GetMapping("/orders")
-    public String getOrders() {
-        return "-0-";
+    @GetMapping("products/allProductsSortedByName")
+    public List<Product> getAllProductsSortedByName() {
+        return productRepository.getAllSortedByName();
     }
 
+    @GetMapping("products/allProductsSortedByPrice")
+    public List<Product> getAllProductsSortedByPrice() {
+        return productRepository.getAllSortedByPrice();
+    }
 
+    @GetMapping(value = "products/allProductsWithGroupSortedByName/{group}"
+            , produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Product> getAllProductsWithGroupSortedByName(@PathVariable String group) {
+        return productRepository.getAllWithGroupSortedByName(group);
+    }
 
+    @GetMapping(value = "products/allProductsWithGroupSortedByPrice/{group}"
+            , produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Product> getAllProductsWithGroupSortedByPrice(@PathVariable String group) {
+        return productRepository.getAllWithGroupSortedByPrice(group);
+    }
+
+    @GetMapping(value = "/products/allProductsSortedByNameWithNameLike/{name}"
+            , produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Product> getAllProductsSortedByNameWithNameLike(@PathVariable String name) {
+        return productRepository.getAllSortedByNameWithNameLike(name);
+    }
+
+    @GetMapping(value = "/products/allProductsSortedByNameWithNameLikeAndTrueStoreStatus/{name}"
+            , produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Product> getAllSortedByNameWithNameLikeAndTrueStoreStatus(@PathVariable String name) {
+        return productRepository.getAllSortedByNameWithNameLikeAndTrueStoreStatus(name);
+    }
+
+    @GetMapping(value = "/products/allProductsSortedByNameWithNameLikeAndDiscount/{name}"
+            , produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Product> getAllSortedByNameWithNameLikeAndDiscount(@PathVariable String name) {
+        return productRepository.getAllSortedByNameWithNameLikeAndNotNullDiscount(name);
+    }
+
+    @GetMapping(value = "/products/allProductsSortedByNameWithNameLikeAndTrueStoreStatusAndDiscount/{name}"
+            , produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Product> getAllSortedByNameWithNameLikeAndTrueStoreStatusAndDiscount(@PathVariable String name) {
+        return productRepository.getAllSortedByNameWithNameLikeAndTrueStoreStatusAndNotNullDiscount(name);
+    }
 }
--- src/main/java/com/mbicycle/imarket/daos/ProfileRepository.java
+++ src/main/java/com/mbicycle/imarket/daos/ProfileRepository.java
@@ -2,6 +2,12 @@ package com.mbicycle.imarket.daos;
 
 import com.mbicycle.imarket.beans.entities.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProfileRepository extends JpaRepository<Profile, Integer> {
+
+    @Query("")
+    List<Profile> getAllSortedByName();
 }
