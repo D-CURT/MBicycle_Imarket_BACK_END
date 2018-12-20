4bf894e7bef1aa3724b096ed13d384f63086339f
Админ <decurt_gogencolern@mail.ru>
2018-12-20 12:24:56 +0300

    ComparatorsConfigurations updated;

--- src/main/java/com/mbicycle/imarket/utils/comparators/ComparatorsConfiguration.java
+++ src/main/java/com/mbicycle/imarket/utils/comparators/ComparatorsConfiguration.java
@@ -14,21 +14,16 @@ import java.util.Set;
 public class ComparatorsConfiguration {
 
     @Bean
-    public Map<String, Comparator> comparators() {
+    public Map<String, Comparator> comparators() throws IllegalAccessException, InstantiationException {
         Reflections reflections = new Reflections("com.mbicycle.imarket.utils.comparators");
         Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(EntityComparator.class);
-        Map<String, Comparator> comparators;
+        Map<String, Comparator> comparators = null;
         if (annotated.size() > 0) {
             comparators = new HashMap<>(annotated.size());
-            annotated.forEach(aClass -> {
-                try {
-                    comparators.put(aClass.getSimpleName(), (Comparator) aClass.newInstance());
-                } catch (InstantiationException | IllegalAccessException ignored) {
-
-                }
-            });
-            return comparators;
+            for (Class aClass: annotated) {
+                comparators.put(aClass.getSimpleName(), (Comparator) aClass.newInstance());
+            }
         }
-        return null;
+        return comparators;
     }
 }
