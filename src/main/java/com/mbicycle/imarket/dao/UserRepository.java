ba752a06723a355c630f22a0d8972d5fd77bea8f
cawendish <Kjvjyjcjd26>
2018-12-20 14:28:06 +0300

    Old version of UserRepository deleted;

--- src/main/java/com/mbicycle/imarket/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mbicycle.imarket.dao;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mbicycle.imarket.beans.entities.Users;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-    Users findByName(String name);
-}
