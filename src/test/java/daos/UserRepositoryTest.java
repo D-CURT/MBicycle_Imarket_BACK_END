623cbf8cdc722f740ada8e0281321fe250057d78
NataliyaGladchenko <natali.glasha@gmail.com>
2018-12-21 15:08:42 +0300

    c1

--- src/test/java/daos/UserRepositoryTest.java
+++ src/test/java/daos/UserRepositoryTest.java
@@ -3,20 +3,15 @@ package daos;
 import com.mbicycle.imarket.Main;
 import com.mbicycle.imarket.beans.entities.Role;
 import com.mbicycle.imarket.beans.entities.User;
-import com.mbicycle.imarket.controllers.UniversalController;
 import com.mbicycle.imarket.daos.UserRepository;
-import com.mbicycle.imarket.services.UserService;
 import com.mbicycle.imarket.utils.RoleType;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,7 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
