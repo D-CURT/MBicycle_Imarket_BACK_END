d3324d6909915fc05f6f4535d25804bda8cc7355
Админ <decurt_gogencolern@mail.ru>
2018-12-21 12:46:04 +0300

    Profile testing in progress;

--- src/main/java/com/mbicycle/imarket/beans/entities/User.java
+++ src/main/java/com/mbicycle/imarket/beans/entities/User.java
@@ -16,7 +16,7 @@ public class User {
 
     private String password;
 
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "id_user"),
--- src/main/java/com/mbicycle/imarket/daos/ProfileRepository.java
+++ src/main/java/com/mbicycle/imarket/daos/ProfileRepository.java
@@ -1,12 +1,15 @@
 package com.mbicycle.imarket.daos;
 
 import com.mbicycle.imarket.beans.entities.Profile;
+import com.mbicycle.imarket.beans.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProfileRepository extends JpaRepository<Profile, Integer> {
+
+    Profile findByUser(User user);
 /*
     @Query("")
     List<Profile> getAllSortedByName();*/
