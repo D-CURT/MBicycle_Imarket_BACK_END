fc700c742a70a2d8d5cf5168a469babcb0fd3346
cawendish <Kjvjyjcjd26>
2018-12-22 13:47:26 +0300

    Merge branch 'develop' of https://bitbucket.org/Mbicycle_bench/mbicycle_imarket into Ihar-Sopat
    # Conflicts:
    #	src/main/java/com/mbicycle/imarket/Main.java
    #	src/main/java/com/mbicycle/imarket/beans/entities/Coupon.java
    #	src/main/java/com/mbicycle/imarket/beans/entities/Product.java
    #	src/main/java/com/mbicycle/imarket/daos/OrderProductRepository.java
    #	src/main/java/com/mbicycle/imarket/utils/generators/ProductGenerator.java
    #	src/main/resources/Diagram/v10.xml

--- src/main/java/com/mbicycle/imarket/utils/generators/ApiExample.java
+++ src/main/java/com/mbicycle/imarket/utils/generators/ApiExample.java
@@ -11,7 +11,7 @@ public class ApiExample {
 
         //Example BaseGenerator API
         BaseGenerator generator = new BaseGenerator();
-        List<Product> allProduct = generator.generateProduсts(1000); //parameter - count. It creates 2 categories with 2 groups, total = 2 * 2 * count
+        List<Product> allProduct = generator.generateProduсts(1000); //parameter - count. It creates 2 categories with 2 groups, total = 2 * 2 * count = 2*2*1000 = 4000 products
         List<User> allUsers = generator.generateUsers(1000); // parameter - count. Simultaneously, profiles are generated
         List<Order> allOrders = generator.generateOrders(15); // parameter - percent!!! Always it have to be past generateUsers(), because they transmit profiles
 
