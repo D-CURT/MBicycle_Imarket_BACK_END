7b2a78a929205ad494bc97bbe114e82af1fb8d4c
Nataliya <natali.glasha@gmail.com>
2018-12-25 15:12:26 +0300

    a1

--- src/main/java/com/mbicycle/imarket/Main.java
+++ src/main/java/com/mbicycle/imarket/Main.java
@@ -1,14 +1,7 @@
 package com.mbicycle.imarket;
 
-import com.mbicycle.imarket.beans.entities.Order;
-import com.mbicycle.imarket.beans.entities.Product;
-import com.mbicycle.imarket.beans.entities.Profile;
-import com.mbicycle.imarket.beans.entities.User;
-import com.mbicycle.imarket.utils.generators.BaseGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Main {
