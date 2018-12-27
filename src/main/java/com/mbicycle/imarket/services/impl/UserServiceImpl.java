95a280e05cab23ce3bc1ba042577a0bc82aef0ad
cawendish <Kjvjyjcjd26>
2018-12-27 14:39:51 +0300

    security2

--- src/main/java/com/mbicycle/imarket/services/impl/UserServiceImpl.java
+++ src/main/java/com/mbicycle/imarket/services/impl/UserServiceImpl.java
@@ -1,37 +1,60 @@
 package com.mbicycle.imarket.services.impl;
 
+import com.mbicycle.imarket.beans.entities.Role;
 import com.mbicycle.imarket.beans.entities.User;
+import com.mbicycle.imarket.daos.RoleRepository;
 import com.mbicycle.imarket.daos.UserRepository;
 import com.mbicycle.imarket.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     public User getUser(String login, String password) {
-        return repository.findByLoginAndPassword(login, password);
+        return userRepository.findByLoginAndPassword(login, password);
     }
 
     public boolean addUser(User user) {
         if (getUser(user.getLogin(), user.getPassword()) == null) {
-            repository.save(user);
+            userRepository.save(user);
             return true;
         }
         return false;
     }
 
     public void deleteUser(String login, String password) {
-        repository.deleteByLoginAndPassword(login, password);
+        userRepository.deleteByLoginAndPassword(login, password);
     }
 
     public List<User> findByOrderByLogin(){
-        return repository.findByOrderByLoginAsc();
+        return userRepository.findByOrderByLoginAsc();
+    }
+
+
+
+    @Override
+    public void save(User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        List<Role> roles = new ArrayList<>();
+        roles.add(roleRepository.getOne(1));
+        user.setRoles(roles);
+        userRepository.save(user);
+    }
+
+    @Override
+    public User findByLogin(String login) {
+        return userRepository.findByLogin(login);
     }
 
 }
