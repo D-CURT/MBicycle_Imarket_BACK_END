c9c8f56c5bec196d1e5a5d69618ff8853ca86542
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-12-27 21:27:32 +0300

    Code corrections; Web ,odule deleted

--- src/main/java/com/mbicycle/imarket/Main.java
+++ src/main/java/com/mbicycle/imarket/Main.java
@@ -10,9 +10,4 @@ public class Main {
     public static void main(String[] args) {
       SpringApplication.run(Main.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return new DataBaseFulfiller();
-    }
 }
