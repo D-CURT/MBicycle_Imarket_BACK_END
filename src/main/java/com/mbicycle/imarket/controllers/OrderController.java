f4fc25e3c13fe3aa3da962f62f591e63a5ea01f7
LeftyForDead <LeftyForDead@mail.ru>
2018-12-27 17:11:55 +0300

    Added for Entitiy Order: DTO, Converter, Facade.

--- src/main/java/com/mbicycle/imarket/controllers/OrderController.java
+++ src/main/java/com/mbicycle/imarket/controllers/OrderController.java
@@ -1,9 +1,14 @@
 package com.mbicycle.imarket.controllers;
 
 import com.mbicycle.imarket.beans.entities.Order;
-import com.mbicycle.imarket.services.impl.OrderServiceImpl;
+import com.mbicycle.imarket.dto.OrderDTO;
+import com.mbicycle.imarket.facades.OrderFacade;
+import com.mbicycle.imarket.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,10 +16,19 @@ import java.util.List;
 @RestController
 public class OrderController {
     @Autowired
-    private OrderServiceImpl service;
+    private OrderService service;
+
+    @Autowired
+    private OrderFacade facade;
 
     @GetMapping("/orders/allOrders")
     public List<Order> getAllOrders(){
         return service.getAllOrder();
+    }
+
+    @PostMapping("/orders/open")
+    public ResponseEntity openOrder(){
+        //facade.openOrder(new OrderDTO());
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/mbicycle/imarket/facades/impl/OrderFacadeImpl.java
@@ -0,0 +1,17 @@
+package com.mbicycle.imarket.facades.impl;
+
+import com.mbicycle.imarket.dto.OrderDTO;
+import com.mbicycle.imarket.facades.OrderFacade;
+
+public class OrderFacadeImpl implements OrderFacade {
+
+    public void openOrder(OrderDTO orderDTO) {
+
+    }
+    public void updateOrder(OrderDTO orderDTO) {
+
+    }
+    public void deleteOrder(OrderDTO orderDTO) {
+
+    }
+}
