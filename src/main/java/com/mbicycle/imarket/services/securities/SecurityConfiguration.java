6a2430a313d3cff0c64f50b999c659566fc0c744
cawendish <Kjvjyjcjd26>
2018-12-29 15:28:08 +0300

    Merge branch 'develop' of https://bitbucket.org/Mbicycle_bench/mbicycle_imarket into Ihar-Sopat
    # Conflicts:
    #	src/main/java/com/mbicycle/imarket/controllers/UserController.java
    #	src/main/java/com/mbicycle/imarket/services/securities/SecurityConfiguration.java

--- src/main/java/com/mbicycle/imarket/services/securities/SecurityConfiguration.java
+++ src/main/java/com/mbicycle/imarket/services/securities/SecurityConfiguration.java
@@ -1,8 +1,5 @@
 package com.mbicycle.imarket.services.securities;
 
-import com.mbicycle.imarket.utils.enums.RoleType;
-
-/*
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -66,60 +63,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     }
 
-
-}
-
-*/ //FIXME: Secirity with Basic
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-               .withUser("admin").password(passwordEncoder().encode("admin"))
-                .roles(RoleType.ADMIN.name())
-                .and()
-                .withUser("manager").password(passwordEncoder().encode("manager"))
-                .roles(RoleType.MANAGER.name())
-                .and()
-                .withUser("customer").password(passwordEncoder().encode("customer"))
-                .roles(RoleType.CUSTOMER.name());
-
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.authorizeRequests()
-                .antMatchers("/", "/index").permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                //.anyRequest().authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/indexLogged").permitAll()
-                .failureUrl("/failureurl")
-                .and()
-                .httpBasic()
-                .and().csrf().disable();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
 }
