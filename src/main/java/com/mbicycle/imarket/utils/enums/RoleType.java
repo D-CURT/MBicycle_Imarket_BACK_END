4ced416183c26207f979620b13068f6b158d7a3f
D-CURT <36221315+D-CURT@users.noreply.github.com>
2019-01-04 19:48:44 +0300

    Names() method for RoleType enum added;

--- src/main/java/com/mbicycle/imarket/utils/enums/RoleType.java
+++ src/main/java/com/mbicycle/imarket/utils/enums/RoleType.java
@@ -2,6 +2,8 @@ package com.mbicycle.imarket.utils.enums;
 
 import com.mbicycle.imarket.beans.entities.Role;
 
+import java.util.Arrays;
+
 public enum RoleType {
     CUSTOMER,
     MANAGER,
@@ -9,5 +11,9 @@ public enum RoleType {
 
     public Role getRole() {
         return new Role(this);
+    }
+
+    public static String[] names() {
+        return Arrays.stream(values()).map(Enum::name).toArray(String[]::new);
     }
 }
